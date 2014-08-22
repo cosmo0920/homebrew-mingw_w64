e01c05b48dec86307188db3485eb7938952a38c9
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-23 01:51:50 +0900

    add binutils-mingw32

---
+++ binutils-mingw32.rb
@@ -0,0 +1,28 @@
+require "formula"
+
+class BinutilsMingw32 < Formula
+  homepage "http://mingw-w64.sourceforge.net/"
+  url "http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.bz2"
+  sha256 "e5e8c5be9664e7f7f96e0d09919110ab5ad597794f5b1809871177a0f0f14137"
+
+  depends_on "gcc48" => :build
+
+  def install
+    install_prefix="/usr/local/mingw"
+    args = %W[
+      CC=gcc-4.8
+      CXX=g++-4.8
+      CPP=cpp-4.8
+      LD=gcc-4.8
+      --target=i686-w64-mingw32
+      --disable-werror
+      --disable-multilib
+      --prefix=#{install_prefix}
+      --with-sysroot=#{install_prefix}
+    ]
+
+    system "./configure", *args
+    system "make install-strip"
+  end
+
+end
