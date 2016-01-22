49095ae3efac797a6ca9b7cffd425d18d8592509
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-01-22 20:24:37 +0900

    i686: Bundle winpthread to i686-w64-mingw32-gcc formula

--- i686-w64-mingw32-gcc.rb
+++ i686-w64-mingw32-gcc.rb
@@ -18,6 +18,11 @@ class I686W64Mingw32Gcc < Formula
     sha256 "89356a0aa8cf9f8b9dc8d92bc8dd01a131d4750c3acb30c6350a406316c42199"
   end
 
+  resource "mingw-winpthread" do
+    url "https://downloads.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v4.0.4.tar.bz2"
+    sha256 "89356a0aa8cf9f8b9dc8d92bc8dd01a131d4750c3acb30c6350a406316c42199"
+  end
+
   depends_on "gcc49" => :build
   depends_on "gmp"
   depends_on "mpfr"
@@ -158,5 +163,22 @@ EOS
 
     # restore PATH
     ENV["PATH"] = path
+
+    resource("mingw-winpthread").stage do
+      ENV["LDPATH"] = "#{target_arch}/#{lib}"
+      args = %W[
+        CC=i686-w64-mingw32-gcc
+        CXX=i686-w64-mingw32-g++
+        CPP=i686-w64-mingw32-cpp
+        --host=#{target_arch}
+        --prefix=#{prefix}/#{target_arch}
+      ]
+
+      chdir "mingw-w64-libraries/winpthreads" do
+        system "./configure", *args
+        system "make"
+        system "make install-strip"
+      end
+    end
   end
 end
