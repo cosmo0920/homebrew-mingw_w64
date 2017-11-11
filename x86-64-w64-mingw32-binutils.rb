2f4c667d37d2d5d227035670617a8825e039d6f3
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2017-11-11 21:04:34 +0900

    Update binutils to 2.29

--- x86-64-w64-mingw32-binutils.rb
+++ x86-64-w64-mingw32-binutils.rb
@@ -1,8 +1,9 @@
 class X8664W64Mingw32Binutils < Formula
   desc "Binutils for minimalist GNU for Windows."
   homepage "https://mingw-w64.org"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz"
-  sha256 "cd717966fc761d840d451dbd58d44e1e5b92949d2073d75b73fccb476d772fcf"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.29.tar.gz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.29.tar.gz"
+  sha256 "172e8c89472cf52712fd23a9f14e9bca6182727fb45b0f8f482652a83d5a11b4"
 
   depends_on "gcc" => :build
 
